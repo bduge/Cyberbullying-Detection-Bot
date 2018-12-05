78c9f18e122ce28904e8211ebfcaae77ee1a3298
Bill DuGe <billduge11@gmail.com>
2018-12-04 19:38:33 -0500

    Added Data and Model files
    Added data and model files to workspace

--- cyberbullying.py
+++ cyberbullying.py
@@ -1,107 +1,112 @@
-# Data Processing tools
+# Data processing tools
 import praw
 import pickle
 import pandas
 import numpy
 
-# Machine learning tools
+# Machine Learning Tools
 from sklearn.model_selection import train_test_split
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.naive_bayes import MultinomialNB
+from sklearn.svm import SVC
 
-# Natural language processing tools
-
+# Natural Language Processing Tools
 from nltk.corpus import stopwords
 from nltk.tokenize import word_tokenize
 from nltk.stem.snowball import SnowballStemmer
 import re
 
-class CyberBullyingDetectionEngine:
 
-    ''' Class that deals with training and deploying cyberbullying detection models'''
+class CyberbullyingDetectionEngine:
+    """ Class that deals with training and deploying cyberbullying detection models
+    """
 
-    def _init_(self):
+    def __init__(self):
         self.corpus = None
         self.tags = None
-        self.lexicon = None
+        self.lexicons = None
         self.vectorizer = None
         self.model = None
         self.metrics = None
 
+    class CustomVectorizer:
+        """ Extracts features from text and vectorizes them
+        """
+
+        def __init__(self, lexicons):
+            self.lexicons = lexicons
+
+        def transform(self, corpus):
+            """ Returns a numpy array of word vectors
+            """
+            word_vectors = []
+            for text in corpus:
+                features = []
+                for k, v in self.lexicons.items():
+                    features.append(len([w for w in word_tokenize(text) if w in v]))
+
+                word_vectors.append(features)
+
+            return numpy.array(word_vectors)
+
     def _simplify(self, corpus):
-        """ Takes in a list of strings (corpus) and remove stopwords, convert to lowercase
-        removes non-letter/number characters (like emojis), stems words so that different tenses are treated the same
+        """ Takes in a list of strings and removes stopwords, converts to lowercase,
+            removes non-alphanumeric characters, and stems each word
         """
         stop_words = set(stopwords.words('english'))
         stemmer = SnowballStemmer('english')
 
         def clean(text):
-            text = re.sub('[^a-zA-Z0-9]', ' ',  text)
+            text = re.sub('[^a-zA-Z0-9]', ' ', text)
             words = [stemmer.stem(w) for w in word_tokenize(text.lower()) if w not in stop_words]
             return " ".join(words)
 
         return [clean(text) for text in corpus]
 
+    def _get_lexicon(self, path):
+        """ Takes in a path to a text file and returns a set
+            containing every word in the file
+        """
+        words = set()
+        with open(path) as file:
+            for line in file:
+                words.update(line.strip().split(' '))
+
+        return words
+
     def _model_metrics(self, features, tags):
-        """takes in testing data and return a dictionary of metrics"""
+        """ Takes in testing data and returns a dictionary of metrics
+        """
         tp = 0
         fp = 0
         tn = 0
         fn = 0
 
-        predictions = self.model.precit(features)
+        predictions = self.model.predict(features)
         for r in zip(predictions, tags):
-            if r[0] == 1 and r[1]== 1:
+            if r[0] == 1 and r[1] == 1:
                 tp += 1
             elif r[0] == 1 and r[1] == 0:
-                fp += 1
+                fp += 0
             elif r[0] == 0 and r[1] == 1:
                 fn += 1
-            elif r[0] == 0 and r[0] == 0:
+            else:
                 tn += 1
 
-
-        predictions = self.model.predict(features)
         precision = tp / (tp + fp)
         recall = tp / (tp + fn)
-        return{
+        return {
             'precision': precision,
             'recall': recall,
-            'f1': (2 * precision * recall) / precision + recall
+            'f1': (2 * precision * recall) / (precision + recall)
         }
 
     def load_corpus(self, path, corpus_col, tag_col):
-        """ Takes in a path to pickled pandas dataframe, the name of corpus, the name of corpus column,
-        and the name of tag column, and extracts a tagged corpus
+        """ Takes in a path to a pickled pandas dataframe, the name of the corpus column,
+            and the name of the tag column, and extracts a tagged corpus
         """
-
-        data = pandas.read_pickle(path)[(corpus_col, tag_col)].values
+        data = pandas.read_pickle(path)[[corpus_col, tag_col]].values
         self.corpus = [row[0] for row in data]
-        self.tags = [row[1]for row in data]
+        self.tags = [row[1] for row in data]
 
-    def train_using_bow(self):
-        """Trains a model using Bag of Words (word counts) on the corpus and tags"""
-
-        corpus = self._simplify(self.corpus)
-        self.vectorizer = CountVectorizer()
-        self.vectorizer.fit(corpus)
-
-        bag_of_words  = self.vectorizer.transform(corpus)
-        x_train, x_test, y_train, y_test = train_test_split(bag_of_words, self.tag, test_size=0.2, stratify=self.tags)
-
-        self.model = MultinomialNB()
-        self.model.fit(x_train, y_train)
-
-        self.metrics = self._model_metrics
-
-    def predict(self,corpus):
-        """ Takes in a text corpus and returns predictions """
-
-        x = self.vetorizer.transform(self._simplify(corpus))
-        return self.model.predict(x)
-
-    def evaluates(self):
-        return self.metrics
-
-    
